2eb6952d2ab581c299751346a60b2c1ad4086a61
Bivekich <biveki-dev@yandex.ru>
2025-04-08 01:16:50 +0300

    Удалены файлы docker-compose.yml и Dockerfile, так как они больше не нужны для проекта.

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'protekspec-cms',
+      script: 'npm',
+      args: 'start',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
